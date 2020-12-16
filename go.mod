882fb4873d0660f9b5fa50fb4abcee33159c0839
Lucian Last <lucian@prowarehouse.nl>
2020-12-16 01:29:39 +0100

    Add program

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/lil5/go-mail-cli
+
+go 1.15
+
+require github.com/vanng822/go-premailer v1.9.0
---
+++ main.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+	"text/template"
+
+	"github.com/vanng822/go-premailer/premailer"
+)
+
+var layout *template.Template
+
+func main() {
+
+	layout = buildTemplate()
+	pagesPath := filepath.Join(".", "pages")
+
+	err := filepath.Walk(pagesPath, func(path string, info os.FileInfo, err error) error {
+		inFileName := info.Name()
+		inFileExt := filepath.Ext(path)
+		if info.IsDir() {
+			return nil
+		}
+		if inFileExt != ".htm" {
+			return nil
+		}
+		outFileName := strings.Replace(inFileName, ".htm", ".html", 1)
+
+		buf := new(bytes.Buffer)
+		buildPage(buf, layout, filepath.Join(pagesPath, inFileName))
+		html := cssInline(buf)
+		err = ioutil.WriteFile(filepath.Join(".", "build", outFileName), []byte(html), 0664)
+		fmt.Print(inFileName, " -> ", outFileName, "\n")
+		if err != nil {
+			return err
+		}
+		return nil
+	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func buildTemplate() *template.Template {
+	t, err := template.ParseGlob(filepath.Join(".", "layout", "*"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	return t
+}
+func buildPage(buf *bytes.Buffer, t *template.Template, inFile string) {
+	t, err := t.ParseGlob(filepath.Join(".", "layout", "*.htm"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	page, err := t.ParseFiles(inFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = page.Execute(buf, nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func cssInline(buf *bytes.Buffer) string {
+	prem, err := premailer.NewPremailerFromBytes(buf.Bytes(), premailer.NewOptions())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	html, err := prem.Transform()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return html
+}
